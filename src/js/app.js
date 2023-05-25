3d485f5ac6b2ae309540a60d5f3dad09248109cc
JasurAvazov <jasuravazov4@gmail.com>
2023-05-26 03:08:30 +0500

    about init

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,13 @@
 import * as functions from "./modules/functions.js";
+import SmoothScroll from "smoothscroll-for-websites";
+
+SmoothScroll({
+    animationTime: 800,
+    stepSize: 60,
+    keyboardSupport: true,
+    arrowScroll: 100,
+    touchpadSupport: true,
+});
 
 functions.isWebp();
 
